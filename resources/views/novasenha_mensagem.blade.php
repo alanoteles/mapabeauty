c7520a0ec36f9764afc425b859710686f96169e7
Alano Teles <alanoteles@gmail.com>
2017-05-11 09:34:28 -0300

    New password message - text adjustment

--- resources/views/novasenha_mensagem.blade.php
+++ resources/views/novasenha_mensagem.blade.php
@@ -1,7 +1,7 @@
-<h3>Admin IBA - Nova senha</h3>
+<h3>Admin Mapa Beauty - Nova senha</h3>
 <hr>
 Caro <b>{{ $params['name'] }}</b>
 
-<p>Foi solicitada a geração de uma nova senha para você pelo Administrador do Portal IBA.</p>
+<p>Foi solicitada a geração de uma nova senha para você pelo Administrador do Mapa Beauty.</p>
 <p>Para acessar a área administrativa, a partir de agora utilize a senha : <b>{{ $params['nova_senha'] }}</b></p>
 
